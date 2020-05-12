4488b2c400de3630c7a2ba96de138996dbc09f36
Daniel Chao <dchao@fortinet.com>
2020-05-11 17:34:14 -0700

    add support for single az ha
    Change-Id: Id7305a7608469eada3f42c0d68ed564e6c191d5c

---
+++ aws/6.2/ha-single-az/output.tf
@@ -0,0 +1,24 @@
+output "FGTActiveMGMTPublicIP" {
+  value = aws_eip.MGMTPublicIP.public_ip
+}
+
+output "FGTClusterPublicFQDN" {
+  value = "${join("", list("https://", "${aws_eip.ClusterPublicIP.public_dns}", ":" , "${var.adminsport}"))}"
+}
+
+output "FGTClusterPublicIP" {
+  value = aws_eip.ClusterPublicIP.public_ip
+}
+
+output "FGTPassiveMGMTPublicIP" {
+  value = aws_eip.PassiveMGMTPublicIP.public_ip
+}
+
+output "Username" {
+  value = "admin"
+}
+
+output "Password" {
+  value = aws_instance.fgtactive.id
+}
+
