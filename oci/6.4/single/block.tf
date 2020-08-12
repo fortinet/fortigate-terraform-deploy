63bef4189f231988beaea4b9479f3c973e817c60
Daniel Chao <dchao@fortinet.com>
2020-08-11 18:13:55 -0700

    add fos 6.4 for oci
    Change-Id: I186e2a9b669a9311ecc2cfa28661be46b28c3a9f

---
+++ oci/6.4/single/block.tf
@@ -0,0 +1,14 @@
+resource "oci_core_volume" "vm_volume" {
+  availability_domain = lookup(data.oci_identity_availability_domains.ads.availability_domains[var.availability_domain - 1], "name")
+  compartment_id      = var.compartment_ocid
+  display_name        = "fgt001-vm_volume"
+  size_in_gbs         = var.volume_size
+}
+// Use paravirtualized attachment for now.
+resource "oci_core_volume_attachment" "vm_volume_attach" {
+  attachment_type = "paravirtualized"
+  //attachment_type = "iscsi" //  user needs to manually add the iscsi disk on fos after
+  compartment_id = var.compartment_ocid
+  instance_id    = oci_core_instance.vm.id
+  volume_id      = oci_core_volume.vm_volume.id
+}
---
+++ oci/6.4/single/variables.tf
@@ -0,0 +1,75 @@
+// OCI configuration
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "private_key_path" {}
+variable "fingerprint" {}
+variable "compartment_ocid" {}
+variable "region" {}
+
+variable "mp_listing_id" {
+  default = "ocid1.appcataloglisting.oc1..aaaaaaaam7ewzrjbltqiarxukuk72v2lqkdtpqtwxqpszqqvrm7likfnpt5q" // byol
+}
+
+// 6.4.1  ocid1.image.oc1..aaaaaaaab3v7bcoqsbrlgw6v3warlwb4hyuiowqk3fnxzrik7dqlgmkzr7zq
+variable "mp_listing_resource_id" {
+  default = "ocid1.image.oc1..aaaaaaaab3v7bcoqsbrlgw6v3warlwb4hyuiowqk3fnxzrik7dqlgmkzr7zq"
+}
+
+// Version
+variable "mp_listing_resource_version" {
+  default = "6.4.1_SR-IOV_Paravirtualized_Mode"
+}
+
+// Environment configuration
+variable "vcn_cidr" {
+  type    = string
+  default = "10.1.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  type    = string
+  default = "10.1.0.0/24"
+}
+
+variable "trust_subnet_cidr" {
+  type    = string
+  default = "10.1.1.0/24"
+}
+
+variable "trust_private_ip_primary" {
+  type    = string
+  default = "10.1.1.3"
+}
+
+variable "trust_private_mask" {
+  type    = string
+  default = "255.255.255.0"
+}
+
+variable "instance_shape" {
+  type    = string
+  default = "VM.Standard2.4"
+}
+
+# Choose an Availability Domain (1,2,3)
+variable "availability_domain" {
+  type    = string
+  default = "1"
+}
+
+variable "volume_size" {
+  type    = string
+  default = "50" //GB; you can modify this, can't less than 50
+}
+
+// Bootstrap configuration file
+variable "bootstrap" {
+  type    = string
+  default = "config.conf"
+}
+
+// License file for the FortiGate
+variable "license" {
+  type    = string
+  default = "license.lic"
+}
