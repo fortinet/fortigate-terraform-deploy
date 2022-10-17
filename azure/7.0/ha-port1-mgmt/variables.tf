c21791502dd9b24655f3593e077ae2b4ca51152c
Daniel Chao <dchao@fortinet.com>
2022-10-17 16:59:44 -0700

    update the 7.0 folder to use version 7.0.8

--- azure/7.0/ha-port1-mgmt/variables.tf
+++ azure/7.0/ha-port1-mgmt/variables.tf
@@ -61,14 +61,14 @@ variable "fgtsku" {
   type = map
   default = {
     byol = "fortinet_fg-vm"
-    payg = "fortinet_fg-vm_payg_20190624"
+    payg = "fortinet_fg-vm_payg_2022"
   }
 }
 
 // FOS version
 variable "fgtversion" {
   type    = string
-  default = "7.0.5"
+  default = "7.0.8"
 }
 
 variable "adminusername" {
