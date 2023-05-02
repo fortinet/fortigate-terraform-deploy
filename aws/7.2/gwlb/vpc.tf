a46961310a7622d55fcf96019b00bb279fb55a8c
Daniel Chao <dchao@fortinet.com>
2023-05-02 13:00:21 -0700

    remove the deprecated classiclink attribute

--- aws/7.2/gwlb/vpc.tf
+++ aws/7.2/gwlb/vpc.tf
@@ -3,7 +3,6 @@ resource "aws_vpc" "fgtvm-vpc" {
   cidr_block           = var.vpccidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-  enable_classiclink   = false
   instance_tenancy     = "default"
   tags = {
     Name = "terraform fgt demo"
@@ -33,7 +32,6 @@ resource "aws_vpc" "customer-vpc" {
   cidr_block           = var.csvpccidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-  enable_classiclink   = false
   instance_tenancy     = "default"
   tags = {
     Name = "terraform customer demo"
