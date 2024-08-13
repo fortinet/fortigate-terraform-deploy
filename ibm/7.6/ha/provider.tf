9f21a9a8e8a24819924d059c4b38a5487398b4cd
Daniel Chao <dchao@fortinet.com>
2024-08-13 10:55:12 -0700

    update ibm to use 7.6 version

---
+++ ibm/7.6/ha/provider.tf
@@ -0,0 +1,19 @@
+variable "ibmcloud_api_key" {}
+variable "iaas_classic_username" {}
+variable "iaas_classic_api_key" {}
+
+terraform {
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = "~> 1.36.0"
+    }
+  }
+}
+
+provider "ibm" {
+  ibmcloud_api_key      = var.ibmcloud_api_key
+  region                = var.region
+  iaas_classic_username = var.iaas_classic_username
+  iaas_classic_api_key  = var.iaas_classic_api_key
+}
