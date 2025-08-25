78b237c549f65e3067d647b68f3996469345c44c
Daniel Chao <dchao@fortinet.com>
2025-08-25 15:00:42 -0700

    update the oci 7.4 to use 7.4.8

--- oci/7.4/single/variables.tf
+++ oci/7.4/single/variables.tf
@@ -7,48 +7,48 @@ variable "compartment_ocid" {}
 variable "region" {}
 
 //x86
-//BYOL 7.4.6: ocid1.appcataloglisting.oc1..aaaaaaaam7ewzrjbltqiarxukuk72v2lqkdtpqtwxqpszqqvrm7likfnpt5q 
-//PAYG 7.4.6 2ocpu: ocid1.appcataloglisting.oc1..aaaaaaaaif6zctibx6njnmob2a23l5if7voquhgsfqxi2ftog2yy3jxmuaba
-//PAYG 7.4.6 4ocup: ocid1.appcataloglisting.oc1..aaaaaaaabepjdf2sw2jkr77a7zrbog7ukzxepoexzgkoyvbw2j2jn7l4y7lq
-//PAYG 7.4.6 8ocup: ocid1.appcataloglisting.oc1..aaaaaaaa6e3iscizq3p24bfb5nr4wxsxzc3s6mzpekxgv7f2kse35akhg45q
-//PAYG 7.4.6 16ocup: ocid1.appcataloglisting.oc1..aaaaaaaawyxdympmdyxagwj2kr77zybopywouiivxd7vxfttwftkvnw2lqla
-//PAYG 7.4.6 24ocup:  ocid1.appcataloglisting.oc1..aaaaaaaamc75m7b3rukv6vd573mdrdqnlqabrbhmz5fggvvtalq3ckfl3zqa
+//BYOL 7.4.8:  ocid1.appcataloglisting.oc1..aaaaaaaam7ewzrjbltqiarxukuk72v2lqkdtpqtwxqpszqqvrm7likfnpt5q
+//PAYG 7.4.8 2ocpu: ocid1.appcataloglisting.oc1..aaaaaaaaif6zctibx6njnmob2a23l5if7voquhgsfqxi2ftog2yy3jxmuaba
+//PAYG 7.4.8 4ocup: ocid1.appcataloglisting.oc1..aaaaaaaabepjdf2sw2jkr77a7zrbog7ukzxepoexzgkoyvbw2j2jn7l4y7lq
+//PAYG 7.4.8 8ocup: ocid1.appcataloglisting.oc1..aaaaaaaa6e3iscizq3p24bfb5nr4wxsxzc3s6mzpekxgv7f2kse35akhg45q
+//PAYG 7.4.8 16ocup: ocid1.appcataloglisting.oc1..aaaaaaaawyxdympmdyxagwj2kr77zybopywouiivxd7vxfttwftkvnw2lqla
+//PAYG 7.4.8 24ocup: ocid1.appcataloglisting.oc1..aaaaaaaamc75m7b3rukv6vd573mdrdqnlqabrbhmz5fggvvtalq3ckfl3zqa
 //ARM
-//BYOL 7.4.6: ocid1.appcataloglisting.oc1..aaaaaaaam7ewzrjbltqiarxukuk72v2lqkdtpqtwxqpszqqvrm7likfnpt5q 
-//PAYG 7.4.6 2ocpu: ocid1.appcataloglisting.oc1..aaaaaaaaif6zctibx6njnmob2a23l5if7voquhgsfqxi2ftog2yy3jxmuaba
-//PAYG 7.4.6 4ocup:  ocid1.appcataloglisting.oc1..aaaaaaaabepjdf2sw2jkr77a7zrbog7ukzxepoexzgkoyvbw2j2jn7l4y7lq
-//PAYG 7.4.6 8ocup:  ocid1.appcataloglisting.oc1..aaaaaaaa6e3iscizq3p24bfb5nr4wxsxzc3s6mzpekxgv7f2kse35akhg45q
-//PAYG 7.4.6 16ocup: ocid1.appcataloglisting.oc1..aaaaaaaawyxdympmdyxagwj2kr77zybopywouiivxd7vxfttwftkvnw2lqla
-//PAYG 7.4.6 24ocup: ocid1.appcataloglisting.oc1..aaaaaaaamc75m7b3rukv6vd573mdrdqnlqabrbhmz5fggvvtalq3ckfl3zqa
+//BYOL 7.4.8:  ocid1.appcataloglisting.oc1..aaaaaaaam7ewzrjbltqiarxukuk72v2lqkdtpqtwxqpszqqvrm7likfnpt5q
+//PAYG 7.4.8 2ocpu:  ocid1.appcataloglisting.oc1..aaaaaaaaif6zctibx6njnmob2a23l5if7voquhgsfqxi2ftog2yy3jxmuaba
+//PAYG 7.4.8 4ocup:  ocid1.appcataloglisting.oc1..aaaaaaaabepjdf2sw2jkr77a7zrbog7ukzxepoexzgkoyvbw2j2jn7l4y7lq
+//PAYG 7.4.8 8ocup:  ocid1.appcataloglisting.oc1..aaaaaaaa6e3iscizq3p24bfb5nr4wxsxzc3s6mzpekxgv7f2kse35akhg45q
+//PAYG 7.4.8 16ocup: ocid1.appcataloglisting.oc1..aaaaaaaawyxdympmdyxagwj2kr77zybopywouiivxd7vxfttwftkvnw2lqla
+//PAYG 7.4.8 24ocup: ocid1.appcataloglisting.oc1..aaaaaaaamc75m7b3rukv6vd573mdrdqnlqabrbhmz5fggvvtalq3ckfl3zqa
 
 variable "mp_listing_id" {
   default = "ocid1.appcataloglisting.oc1..aaaaaaaabepjdf2sw2jkr77a7zrbog7ukzxepoexzgkoyvbw2j2jn7l4y7lq" //x86 4 cores
 }
 
 //x86
-//BYOL 7.4.6: ocid1.image.oc1..aaaaaaaarek6z7oszhvr6hur27ue2oakqpc6imp34eezakb33vrtyxkavt4q
-//PAYG 7.4.6 2ocpu: ocid1.image.oc1..aaaaaaaaeyyhcmlf2fbt2wdbyobhebwguy2zzaox6tghscyco5vgnxp75tnq 
-//PAYG 7.4.6 4ocup: ocid1.image.oc1..aaaaaaaacvsilpxrbtyrnaw2qxy2yhouorf3v7etdacyx6slywkgmnalmxpa
-//PAYG 7.4.6 8ocup: ocid1.image.oc1..aaaaaaaafljvjh7homsgsw5ezyvqvth7mgwren77w2he7qori6tjsmhnbxxq
-//PAYG 7.4.6 16ocup: ocid1.image.oc1..aaaaaaaa4ia3qv6u7ciw42mrrpbisiul4lno7nzgb3bgwofrmhhereg4yrca
-//PAYG 7.4.6 24ocup: ocid1.image.oc1..aaaaaaaaqp4a6pnr7zkn3aprd6ky3gii3hw4ys47xmjg6zqspmniziyzvsna
+//BYOL 7.4.8:  ocid1.image.oc1..aaaaaaaashmz5wtows3no5ghone3gujr77zhotjtqucwdqjx5tmx4stdaajq
+//PAYG 7.4.8 2ocpu:  ocid1.image.oc1..aaaaaaaayveqayo24foiwkod4rsiyo7zgori7db57xregc2uippquz5z2f5a
+//PAYG 7.4.8 4ocup:  ocid1.image.oc1..aaaaaaaamm3fpf4dv37erexdxumhotwfs2b2fgjwgwqmdpa3ho7lvgwadhwa
+//PAYG 7.4.8 8ocup:  ocid1.image.oc1..aaaaaaaafon4ayxkghhufoxe3npywvpgv7bqzp2q4lmwtlmf5nbxqyckhila
+//PAYG 7.4.8 16ocup: ocid1.image.oc1..aaaaaaaacoozagjtio2cjxbplytjclf27er66wxicx32dy7vltwonqrw4wkq
+//PAYG 7.4.8 24ocup: ocid1.image.oc1..aaaaaaaadukflcmalmtmvrxhbhoesyh75z2hn3w5jruj54bcjkcdlxpnfzeq
 //ARM
-//BYOL 7.4.6:  ocid1.image.oc1..aaaaaaaav2xhiv46cjchtazwivlywtejrgikyknndnfrsxdum25p5zg6rjuq
-//PAYG 7.4.6 2ocpu:  ocid1.image.oc1..aaaaaaaapnkx52rikkwk6m5d4r2uhaqt7uwqm32yfexonhlropnv244z3xga
-//PAYG 7.4.6 4ocup:  ocid1.image.oc1..aaaaaaaag4ap5r75vprra5xvwkiruwo3lbhy2tmtbzp5h4mqylsalk4tegvq
-//PAYG 7.4.6 8ocup:  ocid1.image.oc1..aaaaaaaak3sgznogu4hskdp3eazmbyxtazcahjv4witfrqybnh7nks745a7q
-//PAYG 7.4.6 16ocup:  ocid1.image.oc1..aaaaaaaaovwh3dcp3po6nyialg2prcyngzkhunwikgdooxatoarw2ft24tga
-//PAYG 7.4.6 24ocup:  ocid1.image.oc1..aaaaaaaay3t3wx34njceip3tlujlu5m53pxf74bjzinaj24fn2mrwyij4otq
+//BYOL 7.4.8:  ocid1.image.oc1..aaaaaaaal5pyfhxjb7imbdtftxwexfg237pwgefy23idjds7th62ltnboctq
+//PAYG 7.4.8 2ocpu:  ocid1.image.oc1..aaaaaaaam2yjhl3etkyvjdsvjdf4yrpl2pnhss6ez4woyd4ktd5hiuuqvsaq
+//PAYG 7.4.8 4ocup:  ocid1.image.oc1..aaaaaaaao3pfypfcsq6snd6xuuw4ipsk6vhe7gn62awke6bqxsqqwc4wpxqa
+//PAYG 7.4.8 8ocup:  ocid1.image.oc1..aaaaaaaahqcdm2trj53xoby6gfbwmtigrea7qj7rh3lgshv4nr6y6bgtmcra
+//PAYG 7.4.8 16ocup:  ocid1.image.oc1..aaaaaaaar6g4wopzw6imymobqeiz6h4dioulrqdlg22fhjegdhwmabyb2y3a
+//PAYG 7.4.8 24ocup:  ocid1.image.oc1..aaaaaaaaqfjiettahkyogmyfbgdpsdgaqlhblt32sk2n5xx3t5vivq354pla
 
 variable "mp_listing_resource_id" {
-  default = "ocid1.image.oc1..aaaaaaaacvsilpxrbtyrnaw2qxy2yhouorf3v7etdacyx6slywkgmnalmxpa" //x86 payg 4 cores
+  default = "ocid1.image.oc1..aaaaaaaamm3fpf4dv37erexdxumhotwfs2b2fgjwgwqmdpa3ho7lvgwadhwa" //x86 payg 4 cores
 }
 
 // Version
-// x64 - 7.4.6_(_X64_)
-// ARM - 7.4.6_(_ARM64_)
+// x64 - 7.4.8_(_X64_)
+// ARM - 7.4.8_(_ARM64_)
 variable "mp_listing_resource_version" {
-  default = "7.4.6_(_X64_)"
+  default = "7.4.8_(_X64_)"
 }
 
 // Environment configuration
